78778dd3587a41a9535c597af1caed5a0b8fb021
devJRL <dev2sponge@gmail.com>
2019-10-24 23:46:12 +0900

    191024 - 기본 자료형(원시타입) : 논리형
    - boolean : 1 bytes (8 bits) // false 기본

---
+++ 01-NewJavaProject/src/ch02/data_type/Basic4_Boolean.java
@@ -0,0 +1,17 @@
+package ch02.data_type;
+
+/*
+ * 기본 자료형(원시타입) : 논리형
+ */
+public class Basic4_Boolean {
+
+	// 참 또는 거짓을 저장하기 위한 논리 1 byte만 사용
+
+	public static void main(String[] args) {
+
+		boolean b = false;
+		// 1 byte (8 bits)
+		// false(거짓)이 기본값
+
+	}
+}
